93937fe3a6c2f92cee70976943da1d2abad91c6d
akumazero <102664326+akumasugi@users.noreply.github.com>
2022-05-25 19:37:20 +0530

    Update Support.ts

--- src/commands/General/Support.ts
+++ src/commands/General/Support.ts
@@ -23,7 +23,7 @@ export default class Command extends BaseCommand {
            MessageType.text
         ))
         const n = [
-            'https://c.tenor.com/3D__CbAhTl8AAAPo/girl-cute.mp4'
+            'https://telegra.ph/file/bb198d808e2be9bfa72ac.mp4'
         ]
         let beckylynch = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url:beckylynch }, MessageType.video, {quoted:M.WAMessage,
